d1e387d987f1b5b53235a453f421396692229c1c
Gines <ginesr@gmail.com>
2012-09-09 15:04:30 -0300

    better sintax for common module

--- lib/Gideon/DBI/Common.pm
+++ lib/Gideon/DBI/Common.pm
@@ -4,19 +4,22 @@ package Gideon::DBI::Common;
 use strict;
 use warnings;
 use Gideon::Error;
+use Gideon::Error::DBI;
 
-sub execute_with_bind {
+sub execute_with_bind_columns {
     
     my $self  = shift;
-    my $dbh   = shift;
-    my $stmt  = shift;
-    my $bind  = shift;
-    my $block = shift;
+    my $args  = {@_};
+    
+    my $dbh   = $args->{'dbh'} || Gideon::Error->throw('missing db handler');
+    my $stmt  = $args->{'query'} || '';
+    my $bind  = $args->{'bind'} || [];
+    my $block = $args->{'block'} || sub {};
     
     my @bind = map { $_ } @{ $bind };
     
-    my $sth  = $dbh->prepare($stmt) or Gideon::Error->throw( $dbh->errstr );
-    my $rows = $sth->execute(@bind) or Gideon::Error->throw( $dbh->errstr );
+    my $sth  = $dbh->prepare($stmt) or Gideon::Error::DBI->throw( $dbh->errstr );
+    my $rows = $sth->execute(@bind) or Gideon::Error::DBI->throw( $dbh->errstr );
     my %row;
 
     $sth->bind_columns( \( @row{ @{ $sth->{NAME_lc} } } ) ) if @{ $sth->{NAME_lc} };
@@ -26,7 +29,7 @@ sub execute_with_bind {
     }
     $sth->finish;
     
-    return;
+    return $rows;
     
 }
 
