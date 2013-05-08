0ff96672d8e6f308a6ae3be8f6a86490d51b0100
Gines <ginesr@gmail.com>
2013-05-08 14:30:07 -0300

    check valid class in save methods too

--- lib/Test/Gideon/DBI/Mock.pm
+++ lib/Test/Gideon/DBI/Mock.pm
@@ -11,9 +11,7 @@ use Test::MockObject;
 use Test::More;
 use Carp qw(cluck);
 
-our @EXPORT = qw(dies_ok lives_ok throws_ok lives_and);
-
-my @known_methods = qw(find_all find delete update function last_inserted_id);
+my @known_methods = qw(find_all find delete update function last_inserted_id save);
 
 extends 'Test::Gideon::Results';
 
@@ -38,28 +36,28 @@ sub prepare {
 
     my $self  = shift;
     my $query = shift;
-    
-    if ( $query =~ /insert into/i or $query =~ /^update/i ) {
-        return $self->prepare_to_insert($query);
-    }
-    
+
     my $candidate_class = $self->find_class_in_stack;
     my $function = $self->find_function_in_stack;
     my $session = $self->get_next_session;
     $times++;
-    
+
     if ( !$session ) {
         Gideon::Error::DBI->throw("Session exhausted for class $candidate_class called using $function()");
     }
-    
+
     Gideon::Error::DBI->throw("Missing class name in mock, did you mean $candidate_class ?") if not $session->{class};
-    
+
     if ($candidate_class and $session->{class} ne $candidate_class) {
         Gideon::Error::DBI->throw("You said $session->{class} and $candidate_class was found");
     }
-    
+
+    if ( $query =~ /insert into/i or $query =~ /^update/i ) {
+        return $self->prepare_to_insert($query,$candidate_class,$function);
+    }
+
     subtest __PACKAGE__ . " - Subtest $times passed" => sub {
-        plan tests => 4;
+        plan tests => 5;
 
         my $result = 0;
         my $msg    = 'found session';
@@ -78,6 +76,7 @@ sub prepare {
         ok( 1,      "$msg results ($result)" );
         ok( 1,      "sessions left $left" );
         is( $session->{class}, $candidate_class, 'class matches');
+        ok( 1,      "function_found $function()" );
 
     };
 
@@ -89,7 +88,7 @@ sub prepare {
     my $colhash = $self->get_columns_hash( $session->{class} );
     my $sth     = Test::MockObject->new();
     my $bound   = [];
-    
+
     $sth->{NAME_lc} = [ map { $_ } @fields ];
     $sth->mock(
         'execute',
@@ -120,16 +119,16 @@ sub prepare {
             }
 
             my $obj = shift @{ $session->{results} };
-            
+
             die 'result has invalid class' if ref $obj ne $session->{class};
-            
+
             foreach my $field ( @fields ) {
-                
+
                 my $accessor = $colhash->{$field};
                 my $value = $obj->$accessor;
-                
+
                 $row{ $field } = $value;
-                
+
                 if ( ref $bound->[$c] eq 'SCALAR' ) {
                     my $bound = $bound->[$c];
                     $$bound = $value;
@@ -153,14 +152,18 @@ sub prepare_to_insert {
 
     my $self = shift;
     my $query = shift;
+    my $class = shift;
+    my $function = shift;
+
     my $sth  = Test::MockObject->new();
 
     subtest __PACKAGE__ . " - Subtest $times passed" => sub {
-        plan tests => 2;
+        plan tests => 4;
 
         ok( $query, 'query: ' . substr( $query, 0, 50 ) . ' ...' );
         ok( 1, 'no need for session' );
-
+        ok( 1, "class found $class" );
+        ok( 1, "function found $function()" );
     };
 
     $sth->mock(
@@ -181,32 +184,33 @@ sub prepare_to_insert {
 }
 
 sub find_function_in_stack {
-    
+
     my $self = shift;
     my $callers = 0;
     my $function_found;
-    
+
     while (my ($package) = caller($callers) ) {
         my %info = &Carp::caller_info($callers);
         if ( exists $info{'sub'} and $info{'sub'} =~ /^Gideon/) {
             foreach my $method (@known_methods) {
                 if ( $info{'sub_name'} =~ /$method/ ) {
                     $function_found = $info{'sub_name'};
-                    $function_found =~ s/.*\:\:(.*?)(\().*/$1/;
+                    $function_found =~ s/(.*?)(\(.*\))/$1/; # remove params
+                    $function_found =~ s/.*\:\:(.*?)/$1/; # remove package name
                 }
             }
         }
         $callers ++;
     }
-    return $function_found    
+    return $function_found
 }
 
 sub find_class_in_stack {
-    
+
     my $self = shift;
     my $callers = 0;
     my $candidate_class;
-    
+
     while (my ($package) = caller($callers) ) {
         my %info = &Carp::caller_info($callers);
         if ( exists $info{'sub'} and $info{'sub'} =~ /^Test::Exception/ and $info{'pack'} eq 'main') {
@@ -216,11 +220,12 @@ sub find_class_in_stack {
             foreach my $method (@known_methods) {
                 if ( $info{'sub_name'} =~ /$method/ ) {
                     my $params = $info{'sub_name'};
-                    $params =~ s/(.*?\()(.*)(\)$)/$2/;
+                    $params =~ s/(.*?\()(.*)(\)$)/$2/; # extract params
                     next unless $params;
                     my @args = split ',', $params;
                     my $pkg = shift @args;
-                    $pkg =~ s/\'//g;
+                    $pkg =~ s/\'//g; # remove single quote from param list
+                    $pkg =~ s/(.*?)=HASH.*/$1/g; # remove references for objects
                     $candidate_class = $pkg;
                 }
             }
@@ -231,16 +236,16 @@ sub find_class_in_stack {
 }
 
 sub columns_with_table_as_list {
-    
+
     my $self = shift;
     my $session = shift;
-    
+
     my $class = $session->{class};
     my $meta = $class->get_all_meta;
     my $table = $class->get_store_destination();
     my @columns = ();
     my %ignored = ();
-    
+
     if ( my $ignore_fields = $session->{ignore} ) {
         %ignored = map {$_=>1} @$ignore_fields;
     }
@@ -251,7 +256,7 @@ sub columns_with_table_as_list {
             push @columns, $meta->{attributes}->{$attribute}->{column};
         }
     }
-    
+
     return map { $table . '.' . $_ } @columns;
 }
 
